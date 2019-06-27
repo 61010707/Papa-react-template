56fe541f757c6bb304b9fb20177d7031431e836c
DrZin <61010707@kmitl.ac.th>
2019-06-27 19:05:13 +0700

    update app

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -25761,7 +25761,7 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var App = function App() {
-  return _react.default.createElement("h1", null, "Hello world");
+  return _react.default.createElement("h1", null, "Hello world in netlify");
 };
 
 var _default = App;
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  return <h1>Hello world in netlify</h1>;
 };
-export default App
+export default App;
